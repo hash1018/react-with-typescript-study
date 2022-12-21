10bc64f8cd914fed511a972f373220d65c5738b1
SeunghoHa <sh.ha1018@gmail.com>
2022-12-21 12:03:46 +0900

    book chapter 6

---
+++ react quick start with typescript book/clock-app/src/App2.tsx
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+import Clock from './Clock2'
+
+const App = () => {
+    const [formatString, setFormatString] = useState<string>("HH:mm:ss");
+    const [clockVisible, setClockVisible] = useState<boolean>(false);
+
+    return (
+        <div className = "boxStyle">
+            <h2>간단한 시계</h2>
+            <button onClick={()=> setClockVisible(!clockVisible)}>시계 토글하기</button>
+            <hr />
+            {clockVisible ? <Clock formatString={formatString} /> : ""}
+        </div>
+    );
+};
+
+export default App;
--- react quick start with typescript book/hook-test-app/src/main.tsx
+++ react quick start with typescript book/hook-test-app/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App2'
+import App from './App9'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <App />
-  </React.StrictMode>
+  //</React.StrictMode>
 )
